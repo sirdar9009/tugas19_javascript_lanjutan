86abf6ce2bcf47f466532d2da33b96a72d0555d6
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 11:49:45 +0700

    Update tugas19.js

--- tugas19.js
+++ tugas19.js
@@ -1,11 +1,11 @@
 var txt1 ="Semangat..."
 console.log(txt1.repeat(5))
 
-var txt2="*****"
-console.log(txt2.repeat(1))
-console.log(txt2.repeat(4))
-console.log(txt2.repeat(8))
-console.log(txt2.repeat(12))
-console.log(txt2.repeat(8))
-console.log(txt2.repeat(4))
-console.log(txt2.repeat(1))
+var txt2="*"
+console.log(txt2.repeat(5))
+console.log(txt2.repeat(20))
+console.log(txt2.repeat(40))
+console.log(txt2.repeat(60))
+console.log(txt2.repeat(40))
+console.log(txt2.repeat(20))
+console.log(txt2.repeat(5))

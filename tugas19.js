9e29969e35a5d81815f7b41aa214fbebf4c3f148
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-21 11:46:34 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+var txt1 ="Semangat..."
+console.log(txt1.repeat(5))
+
+var txt2="*****"
+console.log(txt2.repeat(1))
+console.log(txt2.repeat(4))
+console.log(txt2.repeat(8))
+console.log(txt2.repeat(12))
+console.log(txt2.repeat(8))
+console.log(txt2.repeat(4))
+console.log(txt2.repeat(1))
